1bc0238a77939f0a51a2a4c4b1a0989804714529
lxxorz <2281388237@qq.com>
2021-01-27 14:54:50 +0800

    add SauceCodePro Nerd Font

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "FiraCode Nerd Font Mono:size=14","NotoSerifDisplay Nerd Font Mono:size=14" };
-static const char dmenufont[]       = "FiraMono Nerd Font Mono:size=11";
+static const char *fonts[]          = {"SauceCodePro Nerd Font:size=14"};
+static const char dmenufont[]       = "FiraMono Nerd Font Mono:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 /* static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL }; */
 static const char *dmenucmd[] = {"rofi", "-modi", "drun", "-show-icons", "-show", "drun", NULL};
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *screenshotcmd[] = { "flameshot", "gui", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
